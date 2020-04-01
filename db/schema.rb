42270631a5510490b4f80c6b7c51d53253d8567c
gameplan1975 <gameplan1975@gmail.com>
2020-04-01 05:20:12 +0000

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,26 @@
+
+ActiveRecord::Schema.define(version: 2020_04_01_045223) do
+
+  enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
